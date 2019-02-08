88b0f8f3d26a4f73ded68738082c3b7e4fe14036
Ravi Kumar E <eravi@aureustechsystems.com>
2019-02-08 06:19:53 +0530

    package

---
+++ src/app/app.routing.ts
@@ -0,0 +1,8 @@
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login.component'
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+ ];
+
+export const routing = RouterModule.forRoot(routes);
---
+++ src/app/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-login',
+    template: `
+  <p>
+    Login works!
+  </p>
+`,
+    styles: []
+})
+export class LoginComponent implements OnInit {
+
+    constructor() { }
+  
+    ngOnInit() {
+    }
+  
+  }
+  
